fff444a37883f15a1227b3b1a7255fde4868277c
akrom17ua <149670082+akrom17ua@users.noreply.github.com>
2023-11-20 23:07:33 +0500

    Create counter_widget.dart

---
+++ lib/counter_widget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CounterWidget extends StatefulWidget {
+  const CounterWidget({Key? key}) : super(key: key);
+
+  @override
+  _CounterWidgetState createState() => _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          'You have pushed the button this many times:',
+        ),
+        Text(
+          '$_counter',
+          style: const TextStyle(fontSize: 24),
+        ),
+        ElevatedButton(
+          onPressed: _incrementCounter,
+          child: const Text('Increment'),
+        ),
+      ],
+    );
+  }
+}
